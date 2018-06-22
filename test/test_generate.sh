a7ba8d28a1a494b77242527e6766d14f6e7cf7eb
Miguel Munoz <mmunoz@gmv.com>
2018-06-22 11:51:13 +0200

    Updated test to fix size issue and run ASN.1 unit tests

--- test/test_generate.sh
+++ test/test_generate.sh
@@ -2,13 +2,36 @@ echo "Test for ros2asn1_generate.py"
 echo ""
 
 PYTHONPATH=../python:$PYTHONPATH
+
+echo "Clear old results"
+rm -rf out_asn out_c
 mkdir -p out_asn out_c
+echo "Done."
+echo ""
 
 echo "Run ASN.1 generation"
 ../python/ros2asn1_generate.py out_asn
 echo "Done."
 echo ""
 
-echo "Run ASN.1 compilation to C"
-asn1.exe -c -o out_c -atc out_asn/* taste-types/*
+echo "Patch userdefs-visualization_msgs.asn to reduce size"
+sed -i "s/60/10/g" out_asn/userdefs-visualization_msgs.asn
 echo "Done."
+echo ""
+
+echo "Run ASN.1 compilation to C"
+asn1.exe -c -o out_c -uPER -atc out_asn/* taste-types/*
+echo "Done."
+echo ""
+
+echo "Compile C code"
+cd out_c
+make
+cd ..
+echo "Done."
+echo ""
+
+echo "Run ASN.1 unit tests"
+./out_c/mainprogram
+echo "Done."
+echo ""
